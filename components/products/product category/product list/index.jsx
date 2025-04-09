1e90f9e35f335a49b355a49a96e4ca882bc18489
c00p75 <georgecoopmsapenda@gmail.com>
2025-04-09 17:01:37 +0200

    Remove padding on products page

--- components/products/product category/product list/index.jsx
+++ components/products/product category/product list/index.jsx
@@ -12,7 +12,7 @@ const ProductList = ({filterBy, filter}) => {
   return (
     <div className="product-category-list bg-[var(--shreeji-primary)] h-fit relative flex flex-col">
       {/* Scrollable Container */}
-      <div className="scroll-container overflow-visible md:overflow-hidden relative px-10 pt-10">
+      <div className="scroll-container overflow-visible md:overflow-hidden relative px-5 md:px-10 pt-10">
         <div className="w-full product-containermin-w-full flex flex-col md:grid grid-cols-3 gap-7">
           {categoryProducts.map((product, index) => (
             <ProductPreview product={product} index={index} />  
